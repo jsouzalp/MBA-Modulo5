d36f9753af62f0e8e06ab1de092fa2748353be62
Jairo Azevedo <jsouza.lp@gmail.com>
2025-11-20 10:44:34 -0300

    Segundo teste github actions

--- src/backend/auth-api/src/Auth.API/Program.cs
+++ src/backend/auth-api/src/Auth.API/Program.cs
@@ -14,6 +14,10 @@ internal class Program
 {
     private static async Task Main(string[] args)
     {
+        // 2nd test github actions
+
+
+
         var builder = WebApplication.CreateBuilder(args);
 
         //builder.WebHost.ConfigureKestrel(options =>
